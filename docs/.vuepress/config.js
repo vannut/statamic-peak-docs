cbe1d23967f1c71eec1412c00bd5f3f1b946c719
Rob de Kort <rob@studio1902.nl>
2021-07-29 13:45:24 +0200

    Add social sharing meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,17 @@
 module.exports = {
-    description: 'Statamic Peak, an opinionated starter kit for all your Statamic sites.',
+    description: 'Statamic Peak is an opinionated starter kit for all your bespoke Statamic sites.',
     head: [
-      ['link',
-        {
-          rel: 'icon',
-          href: '/favicon.svg'
-        }
-      ]
+      ['link', { rel: 'icon', href: '/favicon.svg' }],
+      ['link', { rel: 'mask-icon', href: '/favicon.svg', color: '#FF0274' }],
+      ['script', { src: 'https://cdn.usefathom.com/script.js', 'data-site': 'CCKEJUTO', defer: true }],
+      ['meta', { property: 'og:title', content: 'Statamic Peak Docs' }],
+      ['meta', { property: 'og:description', content: 'Statamic Peak is an opinionated starter kit for all your bespoke Statamic sites.' }],
+      ['meta', { property: 'og:image', content: 'https://peak.studio1902.nl/visuals/statamic-peak-og.png' }],
+      ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+      ['meta', { name: 'twitter:site', content: 'studio1902' }],
+      ['meta', { name: 'twitter:title', content: 'Statamic Peak Docs' }],
+      ['meta', { name: 'twitter:description', content: 'Statamic Peak is an opinionated starter kit for all your bespoke Statamic sites.' }],
+      ['meta', { name: 'twitter:image', content: 'https://peak.studio1902.nl/visuals/statamic-peak-twitter.png' }],
     ],
     lang: 'en-US',
     title: 'Statamic Peak Docs',
